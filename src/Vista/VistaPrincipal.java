09603023b4bae01cd8a1170e7b10a34e1dd331dd
LeninC <cordovalenin.44@gmail.com>
2023-02-21 15:45:59 -0500

    Creación de vista principal, clase conexion

--- src/Vista/VistaPrincipal.java
+++ src/Vista/VistaPrincipal.java
@@ -4,6 +4,11 @@
  */
 package Vista;
 
+import java.awt.Color;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
 /**
  *
  * @author Lenin
@@ -26,57 +31,276 @@ public class VistaPrincipal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
+        jToolBar1 = new javax.swing.JToolBar();
+        btnHome = new javax.swing.JButton();
+        btnEmpleado = new javax.swing.JButton();
+        btnCliente = new javax.swing.JButton();
+        btnHabitacion = new javax.swing.JButton();
+        btnReserva = new javax.swing.JButton();
+        btnFactura = new javax.swing.JButton();
+        btnServicio = new javax.swing.JButton();
+        jPanel4 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        lblfechaDiaria = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        lblNameUser = new javax.swing.JLabel();
+        panelPrincipal = new javax.swing.JDesktopPane();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setBackground(new java.awt.Color(255, 255, 255));
+
+        jPanel1 = new javax.swing.JPanel(){
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                Graphics2D g2d = (Graphics2D) g;
+
+                int width = getWidth();
+                int height = getHeight();
+
+                Color color1 = new Color(255,196,0);
+                Color color2 = new Color(198,11,29);
+
+                GradientPaint gp = new GradientPaint(
+                    0, 0, color1,
+                    width , height / 3, color2);
+
+                g2d.setPaint(gp);
+                g2d.fillRect(0, 0, width, height);
+            }
+        };
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+
+        jPanel2.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel2.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
+
+        jToolBar1.setBackground(new java.awt.Color(255, 255, 255));
+        jToolBar1.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
+        jToolBar1.setOrientation(javax.swing.SwingConstants.VERTICAL);
+        jToolBar1.setRollover(true);
+
+        btnHome.setBackground(new java.awt.Color(255, 255, 255));
+        btnHome.setFont(new java.awt.Font("Palatino Linotype", 0, 14)); // NOI18N
+        btnHome.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icono/HomePV.png"))); // NOI18N
+        btnHome.setText("Inicio");
+        btnHome.setFocusable(false);
+        btnHome.setHorizontalTextPosition(javax.swing.SwingConstants.RIGHT);
+        btnHome.setIconTextGap(40);
+        btnHome.setMargin(new java.awt.Insets(5, 5, 5, 45));
+        btnHome.setMaximumSize(new java.awt.Dimension(185, 35));
+        jToolBar1.add(btnHome);
+
+        btnEmpleado.setBackground(new java.awt.Color(255, 255, 255));
+        btnEmpleado.setFont(new java.awt.Font("Palatino Linotype", 0, 14)); // NOI18N
+        btnEmpleado.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icono/empleados.png"))); // NOI18N
+        btnEmpleado.setText("Empleado");
+        btnEmpleado.setFocusable(false);
+        btnEmpleado.setHorizontalTextPosition(javax.swing.SwingConstants.RIGHT);
+        btnEmpleado.setIconTextGap(40);
+        btnEmpleado.setMargin(new java.awt.Insets(5, 5, 5, 20));
+        btnEmpleado.setMaximumSize(new java.awt.Dimension(185, 35));
+        jToolBar1.add(btnEmpleado);
+
+        btnCliente.setBackground(new java.awt.Color(255, 255, 255));
+        btnCliente.setFont(new java.awt.Font("Palatino Linotype", 0, 14)); // NOI18N
+        btnCliente.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icono/Clientes.png"))); // NOI18N
+        btnCliente.setText("Cliente");
+        btnCliente.setFocusable(false);
+        btnCliente.setHorizontalTextPosition(javax.swing.SwingConstants.RIGHT);
+        btnCliente.setIconTextGap(40);
+        btnCliente.setMargin(new java.awt.Insets(5, 5, 5, 35));
+        btnCliente.setMaximumSize(new java.awt.Dimension(185, 35));
+        btnCliente.setMinimumSize(new java.awt.Dimension(150, 35));
+        jToolBar1.add(btnCliente);
+
+        btnHabitacion.setBackground(new java.awt.Color(255, 255, 255));
+        btnHabitacion.setFont(new java.awt.Font("Palatino Linotype", 0, 14)); // NOI18N
+        btnHabitacion.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icono/Habitaciones.png"))); // NOI18N
+        btnHabitacion.setText("Habitación");
+        btnHabitacion.setFocusable(false);
+        btnHabitacion.setHorizontalTextPosition(javax.swing.SwingConstants.RIGHT);
+        btnHabitacion.setIconTextGap(40);
+        btnHabitacion.setMargin(new java.awt.Insets(5, 5, 5, 10));
+        btnHabitacion.setMaximumSize(new java.awt.Dimension(185, 35));
+        jToolBar1.add(btnHabitacion);
+
+        btnReserva.setBackground(new java.awt.Color(255, 255, 255));
+        btnReserva.setFont(new java.awt.Font("Palatino Linotype", 0, 14)); // NOI18N
+        btnReserva.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icono/Reservas.png"))); // NOI18N
+        btnReserva.setText("Reserva");
+        btnReserva.setFocusable(false);
+        btnReserva.setHorizontalTextPosition(javax.swing.SwingConstants.RIGHT);
+        btnReserva.setIconTextGap(40);
+        btnReserva.setMargin(new java.awt.Insets(5, 5, 5, 30));
+        btnReserva.setMaximumSize(new java.awt.Dimension(185, 35));
+        jToolBar1.add(btnReserva);
+
+        btnFactura.setBackground(new java.awt.Color(255, 255, 255));
+        btnFactura.setFont(new java.awt.Font("Palatino Linotype", 0, 14)); // NOI18N
+        btnFactura.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icono/Facturas.png"))); // NOI18N
+        btnFactura.setText("Factura");
+        btnFactura.setFocusable(false);
+        btnFactura.setHorizontalTextPosition(javax.swing.SwingConstants.RIGHT);
+        btnFactura.setIconTextGap(40);
+        btnFactura.setMargin(new java.awt.Insets(5, 5, 5, 35));
+        btnFactura.setMaximumSize(new java.awt.Dimension(185, 35));
+        jToolBar1.add(btnFactura);
+
+        btnServicio.setBackground(new java.awt.Color(255, 255, 255));
+        btnServicio.setFont(new java.awt.Font("Palatino Linotype", 0, 14)); // NOI18N
+        btnServicio.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icono/Servicios.png"))); // NOI18N
+        btnServicio.setText("Servicio");
+        btnServicio.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        btnServicio.setFocusable(false);
+        btnServicio.setHorizontalTextPosition(javax.swing.SwingConstants.RIGHT);
+        btnServicio.setIconTextGap(40);
+        btnServicio.setMargin(new java.awt.Insets(5, 5, 5, 30));
+        btnServicio.setMaximumSize(new java.awt.Dimension(185, 35));
+        jToolBar1.add(btnServicio);
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jToolBar1, javax.swing.GroupLayout.DEFAULT_SIZE, 183, Short.MAX_VALUE)
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(43, 43, 43)
+                .addComponent(jToolBar1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        jPanel4.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel4.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icono/HotelLogo.png"))); // NOI18N
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icono/Calendario.png"))); // NOI18N
+        jLabel2.setToolTipText("Fecha de hoy");
+
+        lblfechaDiaria.setHorizontalTextPosition(javax.swing.SwingConstants.LEFT);
+
+        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icono/Usuario.png"))); // NOI18N
+
+        javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
+        jPanel4.setLayout(jPanel4Layout);
+        jPanel4Layout.setHorizontalGroup(
+            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel4Layout.createSequentialGroup()
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(lblfechaDiaria, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(52, 52, 52)
+                .addComponent(jLabel4)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(lblNameUser, javax.swing.GroupLayout.PREFERRED_SIZE, 105, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+        jPanel4Layout.setVerticalGroup(
+            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jLabel1)
+            .addGroup(jPanel4Layout.createSequentialGroup()
+                .addGap(19, 19, 19)
+                .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(lblfechaDiaria, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel4Layout.createSequentialGroup()
+                        .addGap(10, 10, 10)
+                        .addComponent(lblNameUser, javax.swing.GroupLayout.PREFERRED_SIZE, 15, javax.swing.GroupLayout.PREFERRED_SIZE))))
+        );
+
+        lblfechaDiaria.getAccessibleContext().setAccessibleName("");
+
+        panelPrincipal = new javax.swing.JDesktopPane(){
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                Graphics2D g2d = (Graphics2D) g;
+
+                int width = getWidth();
+                int height = getHeight();
+
+                Color color1 = new Color(255,196,0);
+                Color color2 = new Color(198,11,29);
+
+                GradientPaint gp = new GradientPaint(
+                    0, 0, color1,
+                    width / 2, height , color2);
+
+                g2d.setPaint(gp);
+                g2d.fillRect(0, 0, width, height);
+            }
+        };
+        panelPrincipal.setBackground(new java.awt.Color(255, 255, 255));
+
+        javax.swing.GroupLayout panelPrincipalLayout = new javax.swing.GroupLayout(panelPrincipal);
+        panelPrincipal.setLayout(panelPrincipalLayout);
+        panelPrincipalLayout.setHorizontalGroup(
+            panelPrincipalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 650, Short.MAX_VALUE)
+        );
+        panelPrincipalLayout.setVerticalGroup(
+            panelPrincipalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 422, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, 0)
+                .addComponent(panelPrincipal))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(panelPrincipal)))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(VistaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(VistaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(VistaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(VistaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new VistaPrincipal().setVisible(true);
-            }
-        });
-    }
-
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnCliente;
+    private javax.swing.JButton btnEmpleado;
+    private javax.swing.JButton btnFactura;
+    private javax.swing.JButton btnHabitacion;
+    private javax.swing.JButton btnHome;
+    private javax.swing.JButton btnReserva;
+    private javax.swing.JButton btnServicio;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel4;
+    private javax.swing.JToolBar jToolBar1;
+    private javax.swing.JLabel lblNameUser;
+    private javax.swing.JLabel lblfechaDiaria;
+    private javax.swing.JDesktopPane panelPrincipal;
     // End of variables declaration//GEN-END:variables
 }
